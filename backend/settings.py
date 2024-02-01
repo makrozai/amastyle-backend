f25420ad52ebf7af9d42113ddf84abc189a2264e
makrozai <marco.condoric@gmail.com>
2024-02-01 02:36:04 -0500

    postgres update

--- backend/settings.py
+++ backend/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbamastyle',
+        'USER': 'makrozai',
+        'PASSWORD': 'amastyle',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
